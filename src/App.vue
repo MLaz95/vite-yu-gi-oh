7cfcd38aecbc56380c5ce8e6f1bb1723129bee24
marco <lazzari.marco.ml@gmail.com>
2024-03-04 15:10:15 +0100

    generato array di carte con api

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,40 @@
-<script setup>
+<script>
+  import axios from 'axios';
+  import {store} from './store.js'
   import AppHead from './components/AppHead.vue'
+
+  export default{
+    components:{
+      AppHead,
+    },
+
+    data(){
+      return{
+        store,
+      }
+    },
+
+    created(){
+      axios.get('https://db.ygoprodeck.com/api/v7/cardinfo.php?num=40&offset=0').then(res => {
+        
+        this.store.cards = res.data.data;
+        console.log(this.store);
+      }).catch(err => {
+        console.log(err)
+      })
+    }
+  }
+
 
 </script>
 
 <template>
   <AppHead></AppHead>
+
+  <div class="container">
+    test
+  </div>
+
 
 </template>
 
@@ -12,6 +42,12 @@
 
   @use './styles/general.scss' as *;
 
+  .container{
+    height: 100vh;
+    // test
+    max-width: 1680px;
+    margin: 125px auto 0;
+    background-color: white;
+  }
 
-
-</style>./components/AppHead.vue
+</style>
--- src/components/AppHead.vue
+++ src/components/AppHead.vue
@@ -1,7 +1,7 @@
 <script>
-export default{
-    name: 'AppHead',
-}
+    export default{
+        name: 'AppHead',
+    }
 
 </script>
 
