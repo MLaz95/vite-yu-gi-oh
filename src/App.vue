3453c85306bdb62dead8e2027f35d87c974e5fc1
marco <lazzari.marco.ml@gmail.com>
2024-03-04 13:00:35 +0100

    aggiunto header

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,17 @@
 <script setup>
+  import AppHead from './components/navbar.vue'
 
 </script>
 
 <template>
+  <AppHead></AppHead>
 
 </template>
 
 <style lang="scss">
 
   @use './styles/general.scss' as *;
+
+
 
 </style>
---
+++ src/components/navbar.vue
@@ -0,0 +1,41 @@
+<script>
+export default{
+    name: 'AppHead',
+}
+
+</script>
+
+<template>
+    <header>
+        <div class="logo-wrapper">
+            <img src="../assets/yugioh-logo.png" alt="">
+        </div>
+        <h1>Yu-gi-oh API</h1>
+    </header>
+</template>
+
+<style lang="scss">
+    header{
+        display: flex;
+        align-items: center;
+        gap: 2rem;
+
+        padding: 15px 40px;
+        background-color: white;
+
+        .logo-wrapper{
+            width: 75px;
+            height: 75px;
+
+            img{
+                width: 100%;
+                height: 100%;
+                object-fit: contain;
+            }
+        }
+
+        h1{
+            font-size: 3.5rem;
+        }
+    }
+</style>
